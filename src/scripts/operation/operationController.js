e24c94d0f1cfa027f24dabdc2f55c27920897ce4
fortesl <fortesl@yahoo.com>
2016-07-30 15:39:29 -0500

    Add timer

--- src/scripts/operation/operationController.js
+++ src/scripts/operation/operationController.js
@@ -5,14 +5,15 @@
     'use strict';
 
     angular.module('tirats').controller('operationController',
-        ['mathServices', 'toastr', '$cookies', '$location', '$routeParams',
-            function(mathServices, toastr, $cookies, $location, $routeParams) {
-                var self = this, _previousGoodAnswer=true, _timerStopper;
+        ['mathServices', 'toastr', '$cookies', '$location', '$routeParams', '$scope',
+            function(mathServices, toastr, $cookies, $location, $routeParams, $scope) {
+                var self = this, _previousGoodAnswer=true;
 
                 var _setCookies = function() {
                     $cookies.put(self.page.id+'Score', self.userScore);
                     $cookies.put(self.page.id+'Correct', self.currentCorrect);
                     $cookies.put(self.page.id+'Wrong', self.currentWrong);
+                    $cookies.put(self.page.id+'Time', self.page.timer.value);
                 };
 
                 var _getCookies = function() {
@@ -41,16 +42,13 @@
                             self.currentWrong++;
                         }
                     }
-                    if (self.userScore === 1 && !self.page.timer.isOn) {
-                        _startTimer();
-                    }
                     _previousGoodAnswer = goodAnswer;
                     self.setElementFocus(self.answer.length-1);
                     _setCookies();
                 };
 
                 var _getAnswer = function(operands) {
-                    var _correctAnswer= self.operands[0].value;
+                    var _correctAnswer= operands[0].value;
 
                     for (var i = 1; i< self.numberOfOperands; i++) {
                         if (self.page.operation === 'Addition')
@@ -112,17 +110,34 @@
                 };
 
                 var _resetTimer = function() {
-                    self.page.timer = {isOn: false, value: 0, hours: '00', minutes: '00', seconds: '00'};
+                    self.page.timer = {isOn: false, idleMark: 90};
+                    self.page.timer.value = $cookies.get(self.page.id+'Time') || 0;
+                    _displayTimer();
                 };
 
-                var _startTime = function() {
+                var _startTimer = function() {
                     self.page.timer.isOn = true;
-                    _timerStopper = window.setInterval(function() {
+                    self.page.timer.idle = 0;
+                    var _timerStopper = window.setInterval(function() {
                         self.page.timer.value++;
-                        self.page.timer.hours = self.page.timer.value / 3600;
-                        self.page.timer.minutes = _addZeroToTheLeft(self.page.timer.value / 60);
-                        self.page.timer.seconds = _addZeroToTheLeft(self.page.timer.value);
+                        self.page.timer.idle++;
+                        if (self.page.timer.idle >=self.page.timer.idleMark) {
+                            window.clearInterval(_timerStopper);
+                            self.page.timer.value -= self.page.timer.idleMark;
+                            self.page.timer.isOn = false;
+                            self.page.timer.idle = 0;
+                        }
+                        _displayTimer();
                     }, 1000);
+                };
+
+                var _displayTimer = function() {
+                    self.page.timer.hours = _addZeroToTheLeft(Math.floor(self.page.timer.value / 3600));
+                    self.page.timer.minutes = _addZeroToTheLeft(Math.floor(self.page.timer.value / 60));
+                    self.page.timer.seconds = _addZeroToTheLeft(Math.floor(self.page.timer.value % 60));
+                    if (!$scope.$$phase) {
+                        $scope.$digest();
+                    }
                 };
                 
                 var _addZeroToTheLeft = function(value) {
@@ -131,7 +146,14 @@
                     }
                     return value;
                 };
-                
+
+                self.notIdle = function() {
+                    self.page.timer.idle = 0;
+                    if (!self.page.timer.isOn) {
+                        _startTimer();
+                    }
+                };
+
                 (function() {
                     self.page = $location.search();
                     self.page.operation = $routeParams.operationId;
